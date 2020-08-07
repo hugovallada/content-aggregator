5ff3973283c3171a324df96f3c424a57f0ce3232
Hugo Vallada <valladahugo@gmail.com>
2020-08-07 02:31:26 -0300

    Criadas funçoes scrappers e modificacao das views e templates

--- scrapper/scrap_util.py
+++ scrapper/scrap_util.py
@@ -1,1 +1,40 @@
+from typing import List, Dict
 from bs4 import BeautifulSoup
+import requests
+
+
+def create_soup(url: str) -> BeautifulSoup:
+    res = requests.get(url)
+    return BeautifulSoup(res.text, "html.parser")
+
+
+def scrap_realpython() -> Dict[str, str]:
+    contents = {}
+    url = "https://realpython.com"
+    soup = create_soup(url)
+    href = soup.find_all("div", {"class": "border-0"})
+
+    for index, item in enumerate(href):
+        if index > 2:
+            break
+        contents[href[index].find("h2").get_text()] = url + href[index].find("a").get(
+            "href"
+        )
+
+    return contents
+
+
+def scrap_manjaro_stable_updates() -> Dict[str, str]:
+    contents = {}
+    url = "https://forum.manjaro.org/c/announcements/stable-updates"
+    soup = create_soup(url)
+    href = soup.find_all("a", {"class": "raw-topic-link"})
+
+    for index, item in enumerate(href):
+        if index > 3:
+            break
+        if index == 0:
+            continue
+        contents[href[index].getText()] = href[index].get("href")
+
+    return contents
--- scrapper/views.py
+++ scrapper/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from .scrap_util import scrap_manjaro_stable_updates, scrap_realpython
+
 
 def index(request):
-    return render(request, 'index.html')
+    contents_real_python = scrap_realpython()
+    contents_manjaro_stable = scrap_manjaro_stable_updates()
+    context = {
+        "contents_real_python": contents_real_python,
+        "contents_manjaro_updates": contents_manjaro_stable,
+    }
+    return render(request, "index.html", context)
+
